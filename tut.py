1456ef601a1b919a633cd3537a41fa6126d63485
shalu911 <94682838+shalu911@users.noreply.github.com>
2021-12-26 15:41:16 +0530

    Create tut.py

---
+++ tut.py
@@ -0,0 +1,11 @@
+from tkinter import *
+shalu_d_root=Tk()
+# width and height 
+shalu_d_root.geometry("644x449")
+# width,height
+shalu_d_root.minsize(200,100)
+#maximam width and hight
+shalu_d_root.maxsize (1200 ,900)
+shivay = Label(text="shiv is a good boy")
+shivay.pack()
+shalu_d_root .mainloop()
